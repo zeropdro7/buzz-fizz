acfebc88359553bfee25ead13123c9b312dbd10e
Pedro Rivera <pedro.rivera@qubika.com>
2025-11-26 19:57:32 -0500

    feat: create challenge solution

---
+++ README.md
@@ -0,0 +1,16 @@
+# Challenge
+
+TypeScript boilerplate.
+
+## Installation
+
+```bash
+npm install
+```
+
+## Commands
+
+```bash
+npm run build    # Compile TypeScript to JavaScript
+npm run dev      # Compile and execute
+```
---
+++ main.ts
@@ -0,0 +1,26 @@
+interface RuleStrategy {
+  rule: (n: number) => boolean;
+  keyword: string;
+}
+
+const rules: RuleStrategy[] = [
+  { rule: (n) => n % 51 === 0, keyword: "Rhai" },
+  { rule: (n) => n % 15 === 0, keyword: "FizzBuzz" },
+  { rule: (n) => n % 3 === 0, keyword: "Fizz" },
+  { rule: (n) => n % 5 === 0, keyword: "Buzz" },
+];
+
+const output = (rule: RuleStrategy | undefined, n: number): string =>
+  rule ? rule.keyword : n.toString();
+
+function strategicFizzBuzz(length: number) {
+  for (let i = 1; i <= length; i++) {
+    const rule = rules.find((n) => n.rule(i));
+    console.log(output(rule, i));
+  }
+}
+
+strategicFizzBuzz(200);
+
+// Export for ES Modules
+export {};
